57bae12d9f51df49752b7d0c86a9fb6393e4d8e0
Chase <croossin94@gmail.com>
2019-08-05 10:46:23 -0700

    Add minimum and max rx values for UISlider (#160)

--- CardParts/src/Classes/Card Parts/CardPartSliderView.swift
+++ CardParts/src/Classes/Card Parts/CardPartSliderView.swift
@@ -14,3 +14,18 @@ public class CardPartSliderView : UISlider, CardPartView {
     public var margins: UIEdgeInsets = CardParts.theme.cardPartMargins
     
 }
+
+extension Reactive where Base: CardPartSliderView {
+    
+    public var minimumValue: Binder<Float> {
+        return Binder(self.base) { (sliderView, minimumValue) -> () in
+            sliderView.minimumValue = minimumValue
+        }
+    }
+    
+    public var maximumValue: Binder<Float> {
+        return Binder(self.base) { (sliderView, maximumValue) -> () in
+            sliderView.maximumValue = maximumValue
+        }
+    }
+}
