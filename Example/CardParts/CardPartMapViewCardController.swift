84a3e1515432801771a038354221f0f81ecea45e
Michael VanDyke <michaeljamesvandyke@gmail.com>
2019-10-06 22:16:32 -0400

    Include reactive textfield for MapView example

--- Example/CardParts/CardPartMapViewCardController.swift
+++ Example/CardParts/CardPartMapViewCardController.swift
@@ -10,55 +10,69 @@ import Foundation
 import CardParts
 import RxCocoa
 import RxSwift
+import CoreLocation
 import MapKit
 
 class CardPartMapViewCardController: CardPartsViewController {
     
     let viewModel = ReactiveCardPartMapViewModel()
     
-    let cardPartTextView = CardPartTextView(type: .title)
-    let cardPartMapView = CardPartMapView(location: CLLocation(latitude: 37.430490, longitude: -122.096260), type: .standard)
+    let cardPartTextView = CardPartTextView(type: .normal)
+    let cardPartTextField = CardPartTextField(format: .zipcode)
+    let cardPartMapView = CardPartMapView(type: .standard, CLLocation(latitude: 37.430489, longitude: -122.096260), zoom: 10_000)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        cardPartTextView.text = "This is a CardPartMapView!"
-       
+        cardPartTextView.text = "This is a CardPartMapView"
+        cardPartTextField.placeholder = "Enter Zipcode"
+ 
+        setupCardParts([
+            cardPartTextView,
+            cardPartTextField,
+            cardPartMapView
+        ])
+        
+        setupConstraints()
+        setupBindings()
+    }
+    
+    private func setupConstraints() {
         cardPartMapView.addConstraint(NSLayoutConstraint(item: cardPartMapView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 300))
-                        
-        setupCardParts([cardPartTextView, cardPartMapView])
+    }
+    
+    private func setupBindings() {
         
-        cardPartMapView.mapView.mapType = .hybrid
-        
-        viewModel.location
+        cardPartTextField.rx.text
+            .distinctUntilChanged()
+            .debounce(1, scheduler: MainScheduler.instance)
+            .filter { $0 != nil }
+            .flatMap { self.viewModel.getLocation(from: $0!) }
             .bind(to: cardPartMapView.rx.location)
+            .disposed(by: bag)
+
+        viewModel.zoom
+            .bind(to: cardPartMapView.rx.zoom)
             .disposed(by: bag)
     }
 }
 
 class ReactiveCardPartMapViewModel {
-
-    var location = BehaviorRelay(value: CLLocation(latitude: 37.430490, longitude: -122.096260))
-    var spanInMeters = BehaviorRelay<Double>(value: 1000)
-
-    init() {
-        randomise()
-    }
     
-    func randomise() {
-        Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(self.random), userInfo: nil, repeats: true)
-    }
+    // Outputs
+    var zoom = BehaviorRelay<CardPartMapView.Meters>(value: 10_000)
     
-    @objc func random() {
-        switch Int.random(in: 0...2) {
-        case 0:
-            location.accept(CLLocation(latitude: 37.430490, longitude: -122.096260))
-        case 1:
-            location.accept(CLLocation(latitude: 38.252666, longitude: -85.758453))
-        case 2:
-            location.accept(CLLocation(latitude: 40.712776, longitude: -74.005974))
-        default:
-            return
+    /// Converts an address string to an observable stream of CLLocation
+    func getLocation(from address: String) -> Observable<CLLocation> {
+        return Observable<CLLocation>.create { observer in
+            CLGeocoder().geocodeAddressString(address) { (placeMarks, _) in
+                if let placeMarks = placeMarks,
+                   let location = placeMarks.first?.location {
+                    observer.onNext(location)
+                    observer.onCompleted()
+                }
+            }
+            return Disposables.create()
         }
     }
     
