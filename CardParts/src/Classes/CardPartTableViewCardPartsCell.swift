cd54c9fe5ec4339771b89c1abce64adbd9c17eee
Chris Young <>
2019-10-20 00:08:01 -0700

    CardPartTableViewCardPartsCell.swift

--- CardParts/src/Classes/CardPartTableViewCardPartsCell.swift
+++ CardParts/src/Classes/CardPartTableViewCardPartsCell.swift
@@ -8,6 +8,47 @@
 
 import Foundation
 
+///This will give you the ability to create custom tableView cells out of CardParts. The following code allows you to create a cell:
+///```
+///class MyCustomTableViewCell: CardPartTableViewCardPartsCell {
+///
+///    let bag = DisposeBag()
+///
+///    let attrHeader1 = CardPartTextView(type: .normal)
+///    let attrHeader2 = CardPartTextView(type: .normal)
+///    let attrHeader3 = CardPartTextView(type: .normal)
+///
+///    override public init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+///        super.init(style: style, reuseIdentifier: reuseIdentifier)
+///
+///        selectionStyle = .none
+///
+///        setupCardParts([attrHeader1, attrHeader2, attrHeader3])
+///    }
+///
+///    required init?(coder aDecoder: NSCoder) {
+///        fatalError("init(coder:) has not been implemented")
+///    }
+///
+///    func setData(_ data: MyCustomStruct) {
+///        // Do something in here
+///    }
+///}
+///```
+///If you do create a custom cell, you must register it to the `CardPartTableView`:
+///```
+///tableViewCardPart.tableView.register(MyCustomTableViewCell.self, forCellReuseIdentifier: "MyCustomTableViewCell")
+///```
+///And then as normal, you would bind to your viewModel's data:
+///```
+///viewModel.listData.bind(to: tableViewPart.tableView) { tableView, indexPath, data in
+///    guard let cell = tableView.dequeueReusableCell(withIdentifier: "MyCustomTableViewCell", for: indexPath) as? MyCustomTableViewCell else { return UITableViewCell() }
+///
+///    cell.setData(data)
+///
+///    return cell
+///}
+///```
 open class CardPartTableViewCardPartsCell : UITableViewCell {
 		
 	private var rightTopConstraint: NSLayoutConstraint!
