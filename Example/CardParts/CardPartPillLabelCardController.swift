50581b10669bbeb0e12080f686a8eb3308fa6dba
Eude Lesperance <l.keude96@gmail.com>
2019-10-30 15:33:39 -0500

    Update PillLabel example to use dynamic colors

--- Example/CardParts/CardPartPillLabelCardController.swift
+++ Example/CardParts/CardPartPillLabelCardController.swift
@@ -26,19 +26,13 @@ class CardPartPillLabelCardController: CardPartsViewController {
         label1.text = "Label1"
         label1.verticalPadding = 10
         label1.horizontalPadding = 10
-        label1.backgroundColor = UIColor.black
-        label1.textColor = UIColor.white
         
         label2.text = "Label2"
         label2.verticalPadding = 10
         label2.horizontalPadding = 10
-        label2.backgroundColor = UIColor.black
-        label2.textColor = UIColor.white
         
         label3.verticalPadding = 10
         label3.horizontalPadding = 10
-        label3.backgroundColor = UIColor.black
-        label3.textColor = UIColor.white
         
         stackView.axis = .horizontal
         stackView.spacing = 8
@@ -47,6 +41,13 @@ class CardPartPillLabelCardController: CardPartsViewController {
         stackView.layoutMargins = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
         
         [label1, label2, label3].forEach { label in
+            if #available(iOS 13.0, *) {
+                label.backgroundColor = .tertiarySystemFill
+                label.textColor = .label
+            } else {
+                label.backgroundColor = UIColor.black
+                label.textColor = UIColor.white
+            }
             stackView.addArrangedSubview(label)
         }
         
