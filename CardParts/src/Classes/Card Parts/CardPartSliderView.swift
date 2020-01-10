b22528998943aa0616fc16a2f1090d6eba05eceb
bcarreon <brian_carreon@sbcglobal.net>
2020-01-10 08:04:15 -0800

    Customize slider height and track colors (#220)

--- CardParts/src/Classes/Card Parts/CardPartSliderView.swift
+++ CardParts/src/Classes/Card Parts/CardPartSliderView.swift
@@ -12,7 +12,22 @@ import RxCocoa
 public class CardPartSliderView : UISlider, CardPartView {
     
     public var margins: UIEdgeInsets = CardParts.theme.cardPartMargins
+    public var height: CGFloat
     
+    public init(height: CGFloat = 3.0) {
+        self.height = height
+        super.init(frame: CGRect.zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override public func trackRect(forBounds bounds: CGRect) -> CGRect {
+        let customBounds = CGRect(origin: bounds.origin, size: CGSize(width: bounds.size.width, height: self.height))
+        super.trackRect(forBounds: customBounds)
+        return customBounds
+    }
 }
 
 extension Reactive where Base: CardPartSliderView {
@@ -26,6 +41,18 @@ extension Reactive where Base: CardPartSliderView {
     public var maximumValue: Binder<Float> {
         return Binder(self.base) { (sliderView, maximumValue) -> () in
             sliderView.maximumValue = maximumValue
+        }
+    }
+    
+    public var minimumTrackTintColor: Binder<UIColor> {
+        return Binder(self.base) { (sliderView, minimumTrackTintColor) -> () in
+            sliderView.minimumTrackTintColor = minimumTrackTintColor
+        }
+    }
+    
+    public var maximumTrackTintColor: Binder<UIColor> {
+        return Binder(self.base) { (sliderView, maximumTrackTintColor) -> () in
+            sliderView.maximumTrackTintColor = maximumTrackTintColor
         }
     }
 }
