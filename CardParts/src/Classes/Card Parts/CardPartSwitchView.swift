1a016b74e4e742f2c9c2601bdca408dc8f21e86a
bcarreon1 <brian_carreon@intuit.com>
2020-02-25 16:15:11 -0800

    Create card part switch component

---
+++ CardParts/src/Classes/Card Parts/CardPartSwitchView.swift
@@ -0,0 +1,10 @@
+//
+//  CardPartSwitchView.swift
+//  CardParts
+//
+//  Created by bcarreon1  on 2/25/20.
+//
+
+public class CardPartSwitchView: UISwitch, CardPartView {
+    public var margins: UIEdgeInsets = CardParts.theme.cardPartMargins
+}
