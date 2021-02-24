84c9b5a00ce41f5ca392377669cb779bcadb319f
Ryan Cole <rycole1@gmail.com>
2021-02-24 15:15:09 -0800

    Add beginTime to emitter to fix emitter position

--- CardParts/src/Classes/Card Parts/CardPartConfettiView.swift
+++ CardParts/src/Classes/Card Parts/CardPartConfettiView.swift
@@ -55,6 +55,7 @@ public class CardPartConfettiView: UIView, CardPartView {
         emitter.emitterPosition = CGPoint(x: frame.size.width / 2.0, y: 0)
         emitter.emitterShape = shape
         emitter.emitterSize = CGSize(width: frame.size.width, height: 1)
+        emitter.beginTime = CACurrentMediaTime()
         
         //construct the cells
         var cells = [CAEmitterCell]()
