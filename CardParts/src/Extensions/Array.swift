22a7d3f4ae5f653d9d3b78b8c3bc0a96bf27441a
Manish Kumar <developer.manish915@gmail.com>
2021-10-25 15:13:30 +0530

    Documentation
    Add documentation for method `safeValue`

--- CardParts/src/Extensions/Array.swift
+++ CardParts/src/Extensions/Array.swift
@@ -8,6 +8,9 @@
 import Foundation
 
 extension Array {
+    /// read an array element at a given `index` safely
+    /// - Parameter index: the index of the element to read
+    /// - Returns: array element at a given `index`. `nil` if index is out of range
     func safeValue(at index: Int) -> Element? {
         guard index >= 0, index < count else {
             return nil
